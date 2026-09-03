048d7dc0cf69a1e648df29bd2c12481b280378cb
Fredrik Nilsson <himske@gmail.com>
2026-09-03 14:32:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using ProductListManager;
+
+List<string> products = [];
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("PRODUCT LIST MANAGER");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine();
+Console.WriteLine("Enter product names. (LETTERS-NUMBER)");
+Console.WriteLine();
+Console.WriteLine("Type 'exit' to finish.");
+Console.WriteLine();
+
+while (true) {
+    Console.Write("Product: ");
+    string? productName = Console.ReadLine();
+
+    productName ??= "";  // Just to make my editor happy
+
+    if (productName.Trim().ToLower().Equals("exit")) {
+        break;
+    }
+
+    productName = productName.ToUpper();
+
+    List<string>  errors = Validation.ValidateInput(productName);
+
+    if (errors.Count > 0) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        foreach (string error in errors) {
+            Console.WriteLine(error);
+        }
+    }
+    else {
+        if (products.Count == 0) {
+            products.Add(productName);
+        }
+        else {
+            foreach (string product in products) {
+                if (string.Equals(productName, product)) {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("WARNING! Product already exists.");
+                }
+                else {
+                    products.Add(productName);
+                }
+            }
+        }
+    }
+
+    Console.ResetColor();
+}
+
+products.Sort();
+
+Console.WriteLine();
+Console.WriteLine("Sorted product list:");
+Console.WriteLine();
+foreach (string product in products) {
+    Console.WriteLine($"- {product}");
+}
+
+Console.Write("Press <Enter> to continue...");
+Console.ReadLine();
---
+++ Validation.cs
@@ -0,0 +1,84 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProductListManager
+{
+    public static class Validation
+    {
+        private static bool HasOnlyOneDash(string input, out string error) {
+            string[] inputParts = input.Split('-');
+            if (inputParts.Length == 1) {
+                error = "Product must contain a dash (-).";
+                return false;
+            }
+
+            if (inputParts.Length > 2) {
+                error = "Product can't have more than 1 dash (-).";
+                return false;
+            }
+            error = "";
+            return true;
+        }
+
+        private static bool HasOnlyUppercaseCharacters(string input, out string error) {
+            foreach (char c in input) {
+                if (c < 'A' || c > 'Z') {
+                    error ="The left side must contain letters only (A-Z).";
+                    return false;
+                }
+            }
+            error = "";
+            return true;
+        }
+
+        private static bool IsValidNumber(string input, out string error) {
+            if (int.TryParse(input, out int number)) {
+                if (number < 200 || number > 500) {
+                    error = "The numeric part must be between 200 and 500.";
+                    return false;
+                }
+            }
+            else {
+                error = "The right side must contain number only.";
+                return false;
+            }
+            error = "";
+            return true;
+        }
+        public static List<string> ValidateInput(string input)
+        {
+            List<string> errors = [];
+
+            if (string.IsNullOrEmpty(input)) {
+                errors.Add("Input can't be empty!");
+                return errors;
+            }
+
+            if (!HasOnlyOneDash(input, out string error)) {
+                errors.Add(error);
+                return errors;
+            }
+
+            string[] inputParts = input.Split('-');
+
+            if (!string.IsNullOrEmpty(inputParts[0])) {
+                if (!HasOnlyUppercaseCharacters(inputParts[0], out error)) {
+                    errors.Add(error);
+                }
+            }
+            else {
+                errors.Add("The left side can't be empty.");
+            }
+
+            if (!string.IsNullOrEmpty(inputParts[1])) {
+                if (!IsValidNumber(inputParts[1], out error)) {
+                    errors.Add(error);
+                }
+            }
+            else {
+                errors.Add("The right side can't be empty.");
+            }
+
+            return errors;
+        }
+    }
+}
